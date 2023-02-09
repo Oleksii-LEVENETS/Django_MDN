589b801f2f9e157123f720b56399e5ddd408545b
Oleksii LEVENETS <oleksii.levenets@icloud.com>
2023-02-09 10:59:26 +0200

    #4 Django admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,67 @@
-# from django.contrib import admin
+from catalog.models import Author, Book, BookInstance, Genre, Language
+
+from django.contrib import admin
 
 # Register your models here.
+# admin.site.register(Book)
+# admin.site.register(Author)
+admin.site.register(Genre)
+# admin.site.register(BookInstance)
+admin.site.register(Language)
+
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    """
+    Add an inline listing of Book items to the Author detail view
+    using the same approach as we did for Book/BookInstance.
+    """
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BookInline]
+    save_as = True
+
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
+    save_as = True
+
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    """
+    For the BookInstance list view, add code to display the book, status, due back date, and id
+    (rather than the default __str__() text).
+    """
+    list_display = ('id', 'book', 'status', 'due_back',)
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+    save_as = True
